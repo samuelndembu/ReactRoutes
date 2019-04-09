7c76eec456477b8057420dec4b5a887e1a8b0184
Muraura <patmur93@gmail.com>
2019-04-09 22:03:02 +0300

    changes to be saved

---
+++ src/index.js
@@ -0,0 +1,27 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom'
+
+//Components
+import Home from './components/home';
+import Posts from './components/posts';
+import Profile from './components/profiles'
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <header>
+                Header
+            </header>
+            <Route path="/" exact component={Home} />
+            <Route path="/posts" component={Posts} />
+            <Route path="/profile" component={Profile}/>
+        </BrowserRouter> 
+
+    )
+}
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+)
